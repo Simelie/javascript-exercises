a766d5b8b5e59a61a068f6987059e25d509b47f3
Simelie <xoraob@gmail.com>
2021-04-19 21:24:06 +0200

    Write function in exercice repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(message, number) {
+    if (number < 0) return 'ERROR';
+    let res = '';
+    for (let i = 0; i < number; i++){
+        res += message;
+    }
+    return res;
 }
-
 module.exports = repeatString
