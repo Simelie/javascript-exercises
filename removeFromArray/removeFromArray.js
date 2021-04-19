bda256a607642e64a9e381c559a7dd9582e72d42
Simelie <xoraob@gmail.com>
2021-04-19 21:47:48 +0200

    Write function in exercice removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...element) {
+    for (let i = 0; i < array.length; i++){
+        for (let j = 0; j < element.length; j++){
+            if (array[i] === element[j]){
+                array.splice(i, 1);
+                i = 0;
+            }
+        }
+    }
+    return array;
 }
 
 module.exports = removeFromArray
