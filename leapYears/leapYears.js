ee715ef2ef0bcc3b1b8f53e0d54fde94846365ee
Simelie <xoraob@gmail.com>
2021-04-20 07:39:57 +0200

    Write function in exercise lapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(number) {
+    if (number % 4 === 0){
+        if (number % 100 !== 0)
+            return true;
+        else if (number % 400 === 0) return true;
+    }
+    return false;
 }
 
 module.exports = leapYears
